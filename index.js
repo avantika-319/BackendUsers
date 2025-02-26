8902af117888a931183c005bc540b183e887d178
avantika.poddar <avantika.poddar@talencred.com>
2025-02-26 22:24:47 +0530

    feat: Added Api for get all users and get user by id

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    {
+        id:1,
+        username: "octocat",
+        name: "The Octocat",
+        repoCount: 8,
+        location: "San Francisco",
+    },
+    {
+        id:2,
+        username: "gaearon",
+        name: "Dan Abramov",
+        repoCount: 50,
+        location: "London",
+    },
+    {
+        id:3,
+        username: "sophiebits",
+        name: "Sophie Alpert",
+        repoCount: 10,
+        location: "San Francisco",
+    },
+    {
+        id:4,
+        username: "addyosmani",
+        name: "Addy Osmani",
+        repoCount: 42,
+        location: "Mountain View",
+    },
+    {
+        id:5,
+        username: "tj",
+        name: "TJ Holowaychuk",
+        repoCount: 388,
+        location: "Victoria",
+    },
+];
+
+
+app.get("/users", (req,res)=>{
+    res.json({users});
+});
+
+app.get('/users/:id', (req, res) => {
+    let userId = parseInt(req.params.id);
+    let user = users.find((user)=> user.id === userId);
+
+    if(user){
+        res.json({user});
+    }else{
+        res.status(404).json({message: "User not found"});
+    }
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
